bab7b0de9e02ddcf257bf0d14065c7121dd7d982
Ivy-End <admin@ivy-end.com>
2015-04-22 20:52:23 +0800

    lecture 4

--- Minesweeper/Minesweeper/Form_Main.Designer.cs
+++ Minesweeper/Minesweeper/Form_Main.Designer.cs
@@ -28,8 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Main));
+            this.MenuStrip_Main = new System.Windows.Forms.MenuStrip();
+            this.gameGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.beginnerBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.intermediateIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.expertEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripSeparator();
+            this.settingSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.markMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.audioMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.rankRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpHToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TableLayoutPanel_Main = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PictureBox_Timer = new System.Windows.Forms.PictureBox();
+            this.Label_Mine = new System.Windows.Forms.Label();
+            this.Label_Timer = new System.Windows.Forms.Label();
+            this.Timer_Main = new System.Windows.Forms.Timer(this.components);
+            this.MenuStrip_Main.SuspendLayout();
+            this.TableLayoutPanel_Main.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Timer)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MenuStrip_Main
+            // 
+            this.MenuStrip_Main.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MenuStrip_Main.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameGToolStripMenuItem,
+            this.helpHToolStripMenuItem});
+            this.MenuStrip_Main.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip_Main.Name = "MenuStrip_Main";
+            this.MenuStrip_Main.Size = new System.Drawing.Size(726, 28);
+            this.MenuStrip_Main.TabIndex = 0;
+            // 
+            // gameGToolStripMenuItem
+            // 
+            this.gameGToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameNToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.beginnerBToolStripMenuItem,
+            this.intermediateIToolStripMenuItem,
+            this.expertEToolStripMenuItem,
+            this.toolStripMenuItem5,
+            this.settingSToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.markMToolStripMenuItem,
+            this.audioMToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.rankRToolStripMenuItem,
+            this.toolStripMenuItem4,
+            this.exitXToolStripMenuItem});
+            this.gameGToolStripMenuItem.Name = "gameGToolStripMenuItem";
+            this.gameGToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.gameGToolStripMenuItem.Text = "Game(&G)";
+            // 
+            // newGameNToolStripMenuItem
+            // 
+            this.newGameNToolStripMenuItem.Name = "newGameNToolStripMenuItem";
+            this.newGameNToolStripMenuItem.Size = new System.Drawing.Size(186, 24);
+            this.newGameNToolStripMenuItem.Text = "New Game(&N)";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(183, 6);
+            // 
+            // beginnerBToolStripMenuItem
+            // 
+            this.beginnerBToolStripMenuItem.Name = "beginnerBToolStripMenuItem";
+            this.beginnerBToolStripMenuItem.Size = new System.Drawing.Size(186, 24);
+            this.beginnerBToolStripMenuItem.Text = "Beginner(&B)";
+            this.beginnerBToolStripMenuItem.Click += new System.EventHandler(this.beginnerBToolStripMenuItem_Click);
+            // 
+            // intermediateIToolStripMenuItem
+            // 
+            this.intermediateIToolStripMenuItem.Name = "intermediateIToolStripMenuItem";
+            this.intermediateIToolStripMenuItem.Size = new System.Drawing.Size(186, 24);
+            this.intermediateIToolStripMenuItem.Text = "Intermediate(&I)";
+            this.intermediateIToolStripMenuItem.Click += new System.EventHandler(this.intermediateIToolStripMenuItem_Click);
+            // 
+            // expertEToolStripMenuItem
+            // 
+            this.expertEToolStripMenuItem.Name = "expertEToolStripMenuItem";
+            this.expertEToolStripMenuItem.Size = new System.Drawing.Size(186, 24);
+            this.expertEToolStripMenuItem.Text = "Expert(&E)";
+            this.expertEToolStripMenuItem.Click += new System.EventHandler(this.expertEToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(183, 6);
+            // 
+            // settingSToolStripMenuItem
+            // 
+            this.settingSToolStripMenuItem.Name = "settingSToolStripMenuItem";
+            this.settingSToolStripMenuItem.Size = new System.Drawing.Size(186, 24);
+            this.settingSToolStripMenuItem.Text = "Setting(&S)";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(183, 6);
+            // 
+            // markMToolStripMenuItem
+            // 
+            this.markMToolStripMenuItem.Name = "markMToolStripMenuItem";
+            this.markMToolStripMenuItem.Size = new System.Drawing.Size(186, 24);
+            this.markMToolStripMenuItem.Text = "Mark(?)(&M)";
+            this.markMToolStripMenuItem.Click += new System.EventHandler(this.markMToolStripMenuItem_Click);
+            // 
+            // audioMToolStripMenuItem
+            // 
+            this.audioMToolStripMenuItem.Name = "audioMToolStripMenuItem";
+            this.audioMToolStripMenuItem.Size = new System.Drawing.Size(186, 24);
+            this.audioMToolStripMenuItem.Text = "Audio(&M)";
+            this.audioMToolStripMenuItem.Click += new System.EventHandler(this.audioMToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(183, 6);
+            // 
+            // rankRToolStripMenuItem
+            // 
+            this.rankRToolStripMenuItem.Name = "rankRToolStripMenuItem";
+            this.rankRToolStripMenuItem.Size = new System.Drawing.Size(186, 24);
+            this.rankRToolStripMenuItem.Text = "Rank(&R)";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(183, 6);
+            // 
+            // exitXToolStripMenuItem
+            // 
+            this.exitXToolStripMenuItem.Name = "exitXToolStripMenuItem";
+            this.exitXToolStripMenuItem.Size = new System.Drawing.Size(186, 24);
+            this.exitXToolStripMenuItem.Text = "Exit(&X)";
+            this.exitXToolStripMenuItem.Click += new System.EventHandler(this.exitXToolStripMenuItem_Click);
+            // 
+            // helpHToolStripMenuItem
+            // 
+            this.helpHToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutAToolStripMenuItem});
+            this.helpHToolStripMenuItem.Name = "helpHToolStripMenuItem";
+            this.helpHToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.helpHToolStripMenuItem.Text = "Help(&H)";
+            // 
+            // aboutAToolStripMenuItem
+            // 
+            this.aboutAToolStripMenuItem.Name = "aboutAToolStripMenuItem";
+            this.aboutAToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.aboutAToolStripMenuItem.Text = "About(&A)";
+            this.aboutAToolStripMenuItem.Click += new System.EventHandler(this.aboutAToolStripMenuItem_Click);
+            // 
+            // TableLayoutPanel_Main
+            // 
+            this.TableLayoutPanel_Main.ColumnCount = 9;
+            this.TableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.TableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.TableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+            this.TableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.TableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+            this.TableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.TableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.TableLayoutPanel_Main.Controls.Add(this.pictureBox1, 1, 0);
+            this.TableLayoutPanel_Main.Controls.Add(this.PictureBox_Timer, 7, 0);
+            this.TableLayoutPanel_Main.Controls.Add(this.Label_Mine, 3, 0);
+            this.TableLayoutPanel_Main.Controls.Add(this.Label_Timer, 5, 0);
+            this.TableLayoutPanel_Main.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.TableLayoutPanel_Main.Location = new System.Drawing.Point(0, 434);
+            this.TableLayoutPanel_Main.Name = "TableLayoutPanel_Main";
+            this.TableLayoutPanel_Main.RowCount = 1;
+            this.TableLayoutPanel_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TableLayoutPanel_Main.Size = new System.Drawing.Size(726, 48);
+            this.TableLayoutPanel_Main.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Minesweeper.Properties.Resources.Mine_Show;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(42, 42);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PictureBox_Timer
+            // 
+            this.PictureBox_Timer.BackgroundImage = global::Minesweeper.Properties.Resources.Timer;
+            this.PictureBox_Timer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PictureBox_Timer.Location = new System.Drawing.Point(649, 3);
+            this.PictureBox_Timer.Name = "PictureBox_Timer";
+            this.PictureBox_Timer.Size = new System.Drawing.Size(42, 42);
+            this.PictureBox_Timer.TabIndex = 1;
+            this.PictureBox_Timer.TabStop = false;
+            // 
+            // Label_Mine
+            // 
+            this.Label_Mine.AutoSize = true;
+            this.Label_Mine.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Label_Mine.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Mine.ForeColor = System.Drawing.Color.DarkRed;
+            this.Label_Mine.Location = new System.Drawing.Point(99, 0);
+            this.Label_Mine.Name = "Label_Mine";
+            this.Label_Mine.Size = new System.Drawing.Size(245, 48);
+            this.Label_Mine.TabIndex = 2;
+            this.Label_Mine.Text = "Label_Mine";
+            this.Label_Mine.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label_Timer
+            // 
+            this.Label_Timer.AutoSize = true;
+            this.Label_Timer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Label_Timer.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Timer.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Label_Timer.Location = new System.Drawing.Point(382, 0);
+            this.Label_Timer.Name = "Label_Timer";
+            this.Label_Timer.Size = new System.Drawing.Size(245, 48);
+            this.Label_Timer.TabIndex = 3;
+            this.Label_Timer.Text = "Label_Timer";
+            this.Label_Timer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form_Main
             // 
@@ -37,16 +269,52 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(726, 482);
+            this.Controls.Add(this.TableLayoutPanel_Main);
+            this.Controls.Add(this.MenuStrip_Main);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.MenuStrip_Main;
             this.MaximizeBox = false;
             this.Name = "Form_Main";
             this.Text = "Minesweeper";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form_Main_Paint);
+            this.MenuStrip_Main.ResumeLayout(false);
+            this.MenuStrip_Main.PerformLayout();
+            this.TableLayoutPanel_Main.ResumeLayout(false);
+            this.TableLayoutPanel_Main.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Timer)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip MenuStrip_Main;
+        private System.Windows.Forms.ToolStripMenuItem gameGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameNToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem beginnerBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem intermediateIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem expertEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem markMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem audioMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem rankRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem exitXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpHToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem settingSToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel TableLayoutPanel_Main;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox PictureBox_Timer;
+        private System.Windows.Forms.Label Label_Mine;
+        private System.Windows.Forms.Label Label_Timer;
+        private System.Windows.Forms.Timer Timer_Main;
     }
 }
 
--- Minesweeper/Minesweeper/Form_Main.cs
+++ Minesweeper/Minesweeper/Form_Main.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,199 @@ namespace Minesweeper
 {
     public partial class Form_Main : Form
     {
+        int nWidth;     // 表示雷区的宽度
+        int nHeight;        // 表示雷区的高度
+        int nMineCnt;       // 表示地雷的数目
+
+        bool bMark;     // 表示是否使用标记
+        bool bAudio;    // 表示是否使用音效
+
         public Form_Main()
         {
             InitializeComponent();
+
+            // 初始化游戏参数
+            nWidth = Properties.Settings.Default.Width;
+            nHeight = Properties.Settings.Default.Height;
+            nMineCnt = Properties.Settings.Default.MineCnt;
+
+            // 初始化
+            bMark = Properties.Settings.Default.Mark;
+            bAudio = Properties.Settings.Default.Audio;
+            markMToolStripMenuItem.Checked = bMark;
+            audioMToolStripMenuItem.Checked = bAudio;
+
+            UpdateSize();
+            SelectLevel();            
+        }
+
+        /// <summary>
+        /// 游戏参数设置
+        /// </summary>
+        /// <param name="Width">雷区宽度</param>
+        /// <param name="Height">雷区高度</param>
+        /// <param name="MineCnt">地雷数目</param>
+        private void SetGame(int Width, int Height, int MineCnt)
+        {
+            nWidth = Width;
+            nHeight = Height;
+            nMineCnt = MineCnt;
+            UpdateSize();
+        }
+
+        /// <summary>
+        /// 设置游戏等级为初级
+        /// </summary>
+        private void SetGameBeginner()
+        {
+            SetGame(10, 10, 10);
+        }
+
+        /// <summary>
+        /// 设置游戏等级为中级
+        /// </summary>
+        private void SetGameIntermediate()
+        {
+            SetGame(16, 16, 40);
+        }
+
+        /// <summary>
+        /// 设置游戏等级为高级
+        /// </summary>
+        private void SetGameExpert()
+        {
+            SetGame(30, 16, 99);
+        }
+
+        /// <summary>
+        /// 选择对应的游戏等级
+        /// </summary>
+        private void SelectLevel()
+        {
+            if (nWidth == 10 && nHeight == 10 && nMineCnt == 10)
+            {
+                beginnerBToolStripMenuItem.Checked = true;
+                intermediateIToolStripMenuItem.Checked = false;
+                expertEToolStripMenuItem.Checked = false;
+                settingSToolStripMenuItem.Checked = false;
+            }
+            else if (nWidth == 16 && nHeight == 16 && nMineCnt == 40)
+            {
+                beginnerBToolStripMenuItem.Checked = false;
+                intermediateIToolStripMenuItem.Checked = true;
+                expertEToolStripMenuItem.Checked = false;
+                settingSToolStripMenuItem.Checked = false;
+            }
+            else if (nWidth == 30 && nHeight == 16 && nMineCnt == 99)
+            {
+                beginnerBToolStripMenuItem.Checked = false;
+                intermediateIToolStripMenuItem.Checked = false;
+                expertEToolStripMenuItem.Checked = true;
+                settingSToolStripMenuItem.Checked = false;
+            }
+            else
+            {
+                beginnerBToolStripMenuItem.Checked = false;
+                intermediateIToolStripMenuItem.Checked = false;
+                expertEToolStripMenuItem.Checked = false;
+                settingSToolStripMenuItem.Checked = true;
+            }
+        }
+
+        private void Form_Main_Paint(object sender, PaintEventArgs e)
+        {
+            PaintGame();
+        }
+
+        /// <summary>
+        /// 绘制游戏区
+        /// </summary>
+        private void PaintGame()
+        {
+            Graphics g = this.CreateGraphics();     //  创建绘图句柄
+            g.FillRectangle(Brushes.White, new Rectangle(0, 0, this.Width, this.Height));
+            // 我们需要是雷区在用户显示的区域上下左右保持6px的偏移量，使得整体看起来更加协调
+            int nOffsetX = 6;   // X方向偏移量
+            int nOffsetY = 6 + MenuStrip_Main.Height;   // Y方向偏移量
+            for (int i = 1; i <= nWidth; i++)    // 绘制行
+            {
+                for (int j = 1; j <= nHeight; j++)   // 绘制列
+                {
+                    // 第一个参数为笔刷，这里采用内置笔刷SandyBronw
+                    // 第二个参数为方块的参数，这里采用左上角坐标以及长宽的形式给出
+                    // 34表示每个雷区的大小，再加上偏移量就是我们当前雷区的起始位置，由于要空出1px的间隔，因此还需要加1
+                    // 由此可以的到每个方块在雷区中的位置，然后利用循环绘制出来
+                    g.FillRectangle(Brushes.SandyBrown, new Rectangle(nOffsetX + 34 * (i - 1) + 1, nOffsetY + 34 * (j - 1) + 1, 32, 32));   // 绘制雷区方块
+                }
+            }
+        }
+
+        /// <summary>
+        /// 自动更新窗口大小
+        /// </summary>
+        private void UpdateSize()
+        {
+            int nOffsetX = this.Width - this.ClientSize.Width;  // 包含了窗口标题栏以及上下边框的高度
+            int nOffsetY = this.Height - this.ClientSize.Height;    // 包含了左右边框的宽度
+            int nAdditionY = MenuStrip_Main.Height + TableLayoutPanel_Main.Height;  // 包含了菜单栏以及下方显示信息栏的高度
+            this.Width = 12 + 34 * nWidth + nOffsetX;   // 设置窗口高度，34为每个雷区的高度，12为上下总空隙（6px+6px），再加上偏移量
+            this.Height = 12 + 34 * nHeight + nAdditionY + nOffsetY;    // 设置窗口宽度，同理
+            PaintGame();
+        }
+
+        private void beginnerBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            nWidth = 10; nHeight = 10; nMineCnt = 10;
+            SelectLevel();
+            UpdateSize();
+        }
+
+        private void intermediateIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            nWidth = 16; nHeight = 16; nMineCnt = 40;
+            SelectLevel();
+            UpdateSize();
+        }
+
+        private void expertEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            nWidth = 30; nHeight = 16; nMineCnt = 99;
+            SelectLevel();
+            UpdateSize();
+        }
+
+        private void exitXToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure to exit the game?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// 系统关于对话框（API）
+        /// </summary>
+        /// <param name="hWnd">窗口句柄</param>
+        /// <param name="szApp">标题文本</param>
+        /// <param name="szOtherStuff">内容文本</param>
+        /// <param name="hIcon">图标句柄</param>
+        /// <returns></returns>
+        [DllImport("shell32.dll")]
+        public extern static int ShellAbout(IntPtr hWnd, string szApp, string szOtherStuff, IntPtr hIcon);
+
+        private void aboutAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShellAbout(this.Handle, "Minesweeper", "A minesweeper game using CSharp language.", this.Icon.Handle);
+        }
+
+        private void markMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            markMToolStripMenuItem.Checked = bMark = !bMark;
+        }
+
+        private void audioMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            audioMToolStripMenuItem.Checked = bAudio = !bAudio;
         }
     }
 }
